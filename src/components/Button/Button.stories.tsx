1cadfc420c26770487b7c50626da413133488b0e
Bruno Moreira <bruno@brunormoreira.com>
2021-07-12 22:24:38 -0300

    Improve Button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import { Meta, Story } from '@storybook/react'
 import { Button, ButtonProps } from './Button'
+
+import { styled } from '@storybook/theming'
 
 const meta: Meta = {
   title: 'Button',
@@ -18,6 +20,21 @@ const meta: Meta = {
         disabled: true,
       },
     },
+    backgroundColor: {
+      control: { type: 'color' },
+    },
+    color: {
+      control: { type: 'color' },
+    },
+    height: {
+      control: { type: 'range', min: 0, max: 10, step: 0.01 },
+    },
+    width: {
+      control: { type: 'range', min: 0, max: 10, step: 0.01 },
+    },
+    borderRadius: {
+      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+    },
   },
   parameters: {
     controls: { expanded: false },
@@ -26,10 +43,29 @@ const meta: Meta = {
 
 export default meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />
+const Template: Story<ButtonProps & CSSProperties> = ({
+  backgroundColor,
+  width,
+  height,
+  color,
+  borderRadius,
+  ...args
+}) => {
+  const StyledButton = styled(Button)`
+    background-color: ${backgroundColor};
+    width: ${width}em;
+    height: ${height}em;
+    border-radius: ${borderRadius}em;
+    border: none;
+    color: ${color};
+  `
+  return <StyledButton {...args} />
+}
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({})
 
-Default.args = {}
+Default.args = {
+  children: 'Button',
+}
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,19 @@
-import React, { FC, HTMLAttributes } from 'react'
+import React, { forwardRef, HTMLAttributes } from 'react'
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   testId?: string
 }
 
-const Button: FC<ButtonProps> = ({ children, testId, ...props }) => {
-  return (
-    <button data-testid={testId} {...props}>
-      {children}
-    </button>
-  )
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, testId, ...props }, ref) => {
+    return (
+      <button ref={ref} data-testid={testId} {...props}>
+        {children}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
 
 export { Button, ButtonProps }
